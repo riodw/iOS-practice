0a623e9c9a0a17f8689dde7600f50c974f0794f6
Rio Weber <riodweber@gmail.com>
2021-12-05 20:35:03 -0500

    BetterRest; WordScramble;

---
+++ BetterRest/Shared/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  Shared
+//
+//  Created by Riorden Weber on 12/2/21.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # iOS-practice
+
+1. WeSplit
+2. GuessTheFlag
+3. BetterRest
+4. WordScramble
