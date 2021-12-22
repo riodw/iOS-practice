480ee60cb075034221fb519ec7ca7ea1dad085da
Rio Weber <riodweber@gmail.com>
2021-12-21 19:50:34 -0500

    Start project Animation;

---
+++ Animations/Shared/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Shared
+//
+//  Created by Riorden Weber on 12/21/21.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
